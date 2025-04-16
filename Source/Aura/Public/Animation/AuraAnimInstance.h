1a1aedfadea3a1d6bcd415abc36cdf6e953006fe
Chalat Srisuwan <sakichan2521@gmail.com>
2025-04-16 21:11:59 +0700

    Add AnimInstance.

---
+++ Source/Aura/Public/Animation/AuraAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AuraAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
